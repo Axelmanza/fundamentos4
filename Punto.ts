9c204b5bd2dbf6586bb9bed56a56380268658b37
Axelmanza <manzanares712@gmail.com>
2020-05-19 15:52:08 +0200

    implementacion calcularCercano

--- Punto.ts
+++ Punto.ts
@@ -12,7 +12,7 @@ export class Punto{
     public distanciaAlOrigen(x = this.x, y = this.y):number{
         return Math.sqrt(Math.pow(x,2) + Math.pow(y,2))
     }
-    public calcularDistancia(B:Punto){
+    public calcularDistancia(B:Punto): number{
         let [x,y] = [B.getX(), B.getY()]
         return this.distanciaAlOrigen(this.x-x, this.y-y)
     }
@@ -28,4 +28,37 @@ export class Punto{
     public setY(y: number){
         this.y = y
     }
+    public calcularCuadrante(): number{
+        let cuadrante: number = 0;
+        if (this.x > 0 && this.y > 0){
+            cuadrante = 1;
+        }
+        else if (this.x < 0 && this.y >= 0){
+            cuadrante = 2;
+        }
+            else if(this.x < 0 && this.y < 0){
+            cuadrante = 3;
+        }
+        else{
+            cuadrante = 4;
+        }
+        return cuadrante;
+    }
+    public masCercano(puntos: Punto[]):Punto{
+        let distancias: number[] = [];
+        let cercano: number = 0;
+        let resultado: number = 0;
+        // array de distancias al punto
+        for (let i = 0; i < puntos.length; i++){
+            distancias.push(this.calcularDistancia(puntos[i]))
+        }
+        // calcula el minimo del array
+        cercano = distancias.reduce((n,m) => Math.min(n,m), Number.POSITIVE_INFINITY);
+        // coge la posicion de la distncia mas cercana
+        resultado = distancias.indexOf(cercano);
+        // devuelve el punto igual a la posicion de la distancia mas cercana
+        return puntos[resultado]
+    }
+    
+
 }
--- main.ts
+++ main.ts
@@ -1,10 +1,15 @@
 import { Punto } from './Punto'
 
-let p1 = new Punto(1,2)
-console.log(p1.toString())
+let p1 = new Punto(-1,-2)
+// console.log(p1.toString())
 
-console.log(p1.distanciaAlOrigen())
+// console.log(p1.distanciaAlOrigen())
 
 let p2 = new Punto(2,2)
 
-console.log(p1.calcularDistancia(p2))
+// console.log(p1.calcularDistancia(p2))
+// console.log(p1.calcularCuadrante());
+
+let p3 = new Punto(10, 10);
+
+console.log(p1.masCercano([p2, p3]));
